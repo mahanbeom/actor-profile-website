af836e5c7226779af6a6e3fc0cdbe61e01e87717
mahanbeom <akgksqja@gmail.com>
2022-11-27 16:47:16 +0900

    routing fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
