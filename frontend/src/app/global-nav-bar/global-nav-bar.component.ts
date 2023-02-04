a97dae7182e42d907d0b4ddf444bacbbeaaab9f5
mahanbeom <akgksqja@gmail.com>
2023-02-04 16:36:01 +0900

    test: api test

--- frontend/src/app/global-nav-bar/global-nav-bar.component.ts
+++ frontend/src/app/global-nav-bar/global-nav-bar.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,13 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalNavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   moveToLink(url: string) {
     window.open(url, '_self');
+  }
+
+  apiTest() {
+    const signInForm = {
+      "email": "akgksqja@gamil.com",
+      "password": "akgksqja1!"
+    };
+
+    this.http.get("https://3stb21askl.execute-api.ap-northeast-2.amazonaws.com/dev/").subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
   }
 
 }
