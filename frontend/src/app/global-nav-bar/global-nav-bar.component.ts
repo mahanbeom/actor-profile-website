dce0ee0d1d61c6227b59a4297fe52f5ed5fe268a
mahanbeom <akgksqja@gmail.com>
2023-02-04 18:05:21 +0900

    remove: api test

--- frontend/src/app/global-nav-bar/global-nav-bar.component.ts
+++ frontend/src/app/global-nav-bar/global-nav-bar.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalNavBarComponent implements OnInit {
 
-  readonly url = "https://3stb21askl.execute-api.ap-northeast-2.amazonaws.com/dev/";
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
@@ -17,42 +15,4 @@ export class GlobalNavBarComponent implements OnInit {
   moveToLink(url: string) {
     window.open(url, '_self');
   }
-
-  private formHeaders(): HttpHeaders {
-    const formHeader = new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'withCredentials': 'true'
-    });
-    return formHeader;
-  }
-
-  login() {
-    const signInForm = {
-      "email": "akgksqja@gmail.com",
-      "password": "akgksqja1!"
-    };
-
-    this.http.post<any>(this.url + "users/login", signInForm, {
-      headers: this.formHeaders(),
-      observe: 'response'
-    }).subscribe((response: HttpResponse<any>) => {
-      console.log(response);
-      const setCookie = response.headers.get(`set-cookie`);
-      console.log(setCookie);
-      document.cookie = `name=`
-    })
-  }
-
-  apiTest() {
-
-    this.http.get(this.url).subscribe({
-      next: (res) => {
-        console.log(res);
-      },
-      error: (err) => {
-        console.log(err);
-      }
-    })
-  }
-
 }
